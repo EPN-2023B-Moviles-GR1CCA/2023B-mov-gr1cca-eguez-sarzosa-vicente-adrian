97ba31ee2808d1670bec1c4e59ded842afbb7c36
Adrian Eguez <eadepto@hotmail.com>
2024-01-04 19:20:55 -0500

    hasta crear actualizar y eliminar

--- 02-android/app/src/main/java/com/example/gr1accvaes2023b/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/gr1accvaes2023b/MainActivity.kt
@@ -62,8 +62,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        // Base de datos sqlite
+        EBaseDeDatos.tablaEntrenador = ESqliteHelperEntrenador(this)
+
         val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida2)
 
         botonCicloVida
